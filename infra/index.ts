47d704068c738a032fc1fd7c958524f96b3b329b
Daniel Levi <daniellevi000@gmail.com>
2025-04-10 13:44:44 -0300

    feat: :sparkles: Initialize Pulumi infrastructure for upload server with AWS S3 bucket

---
+++ infra/index.ts
@@ -0,0 +1,17 @@
+import * as aws from '@pulumi/aws'
+import * as awsx from '@pulumi/awsx'
+import * as pulumi from '@pulumi/pulumi'
+
+// Create an AWS resource (S3 Bucket)
+
+const bucket = new aws.s3.BucketV2('upload-server', {
+  bucket: 'upload-server',
+  tags: {
+    IAC: 'true',
+  },
+})
+
+// Export the name of the bucket
+export const bucketName = bucket.id
+export const bucketArn = bucket.arn
+export const bucketInfo = bucket.bucket
