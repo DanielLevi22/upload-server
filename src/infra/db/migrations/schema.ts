fd401aed60e6b3a1455203de4bf3224812041a2a
Daniel Levi <daniellevi000@gmail.com>
2025-04-14 12:51:30 -0300

    test

--- src/infra/db/migrations/schema.ts
+++ src/infra/db/migrations/schema.ts
@@ -1,15 +1,17 @@
-import { pgTable, unique, text, timestamp } from "drizzle-orm/pg-core"
-import { sql } from "drizzle-orm"
+import { sql } from 'drizzle-orm'
+import { pgTable, text, timestamp, unique } from 'drizzle-orm/pg-core'
 
-
-
-export const uploads = pgTable("uploads", {
-	id: text().primaryKey().notNull(),
-	name: text().notNull(),
-	remoteKey: text("remote_key").notNull(),
-	remoteUrl: text("remote_url").notNull(),
-	createdAt: timestamp("created_at", { mode: 'string' }).defaultNow().notNull(),
-	size: text().notNull(),
-}, (table) => [
-	unique("uploads_remote_key_unique").on(table.remoteKey),
-]);
+export const uploads = pgTable(
+  'uploads',
+  {
+    id: text().primaryKey().notNull(),
+    name: text().notNull(),
+    remoteKey: text('remote_key').notNull(),
+    remoteUrl: text('remote_url').notNull(),
+    createdAt: timestamp('created_at', { mode: 'string' })
+      .defaultNow()
+      .notNull(),
+    size: text().notNull(),
+  },
+  table => [unique('uploads_remote_key_unique').on(table.remoteKey)]
+)
